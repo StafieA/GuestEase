b8a8328b3c2f053712ceb235f7825672c289aaa1
Andrei Stafie <andreistafie@gmail.com>
2025-03-23 15:04:30 +0200

    Connect Supabase with the app

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://bnnoqqutapejleoccnmp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJubm9xcXV0YXBlamxlb2Njbm1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI3MjgxNDUsImV4cCI6MjA1ODMwNDE0NX0.sFhN_YzsLn8qRVEw8oWRFV0PErxLPq4Rrgb3rrvpNBw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
