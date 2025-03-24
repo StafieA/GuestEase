b8506524a92aa3c38413fe1e0f5cbfbd6079d46c
Andrei Stafie <andreistafie@gmail.com>
2025-03-24 11:26:12 +0200

    Delete a cabin

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -31,7 +31,7 @@ const TableHeader = styled.header`
 
 function CabinTable() {
   const { isLoading, data: cabins } = useQuery({
-    queryKey: ["cabin"],
+    queryKey: ["cabins"],
     queryFn: getCabins,
   });
 
