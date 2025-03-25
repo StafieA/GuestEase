1e81233481b3bfa15cb4f2af13aeaefbcbc53c34
Andrei Stafie <andreistafie@gmail.com>
2025-03-25 11:34:16 +0200

    Upload images to supabase

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
@@ -11,32 +11,32 @@ import { createCabin } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 import FormRow from "../../ui/FormRow";
 
-const FormRow2 = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
+// const FormRow2 = styled.div`
+//   display: grid;
+//   align-items: center;
+//   grid-template-columns: 24rem 1fr 1.2fr;
+//   gap: 2.4rem;
 
-  padding: 1.2rem 0;
+//   padding: 1.2rem 0;
 
-  &:first-child {
-    padding-top: 0;
-  }
+//   &:first-child {
+//     padding-top: 0;
+//   }
 
-  &:last-child {
-    padding-bottom: 0;
-  }
+//   &:last-child {
+//     padding-bottom: 0;
+//   }
 
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
 
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
+//   &:has(button) {
+//     display: flex;
+//     justify-content: flex-end;
+//     gap: 1.2rem;
+//   }
+// `;
 
 // const Label = styled.label`
 //   font-weight: 500;
@@ -75,8 +75,10 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    console.log(data);
-    mutate(data);
+    // console.log(data);
+    // console.log(data.image);
+    // console.log(data.image[0]);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -84,17 +86,6 @@ function CreateCabinForm() {
   }
   return (
     <Form onSubmit={handleSubmit(onSubmit, onError)}>
-      {/* <FormRow2>
-        <Label htmlFor="name">Cabin Name</Label>
-        <Input
-          type="text"
-          id="name"
-          {...register("name", {
-            required: "This field is required",
-          })}
-        />
-        {errors?.name?.message && <Error>{errors.name.message}</Error>}
-      </FormRow2> */}
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -105,20 +96,6 @@ function CreateCabinForm() {
           })}
         />
       </FormRow>
-      {/* <FormRow2>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input
-          type="number"
-          id="maxCapacity"
-          {...register("maxCapacity", {
-            required: "This field is required",
-            min: {
-              value: 1,
-              message: "Capacity should be at least 1",
-            },
-          })}
-        />
-      </FormRow2> */}
 
       <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
         <Input
@@ -135,20 +112,6 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      {/* <FormRow2>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input
-          type="number"
-          id="regularPrice"
-          {...register("regularPrice", {
-            required: "This field is required",
-            min: {
-              value: 1,
-              message: "Capacity should be at least 1",
-            },
-          })}
-        />
-      </FormRow2> */}
       <FormRow label="Regular price" error={errors?.regularPrice?.message}>
         <Input
           type="number"
@@ -160,21 +123,6 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      {/* <FormRow2>
-        <Label htmlFor="discount">Discount</Label>
-        <Input
-          type="number"
-          id="discount"
-          defaultValue={0}
-          {...register("discount", {
-            required: "This field is required",
-            validate: (value) => {
-              value < getValues().regularPrice ||
-                "Discount should be less than regular price";
-            },
-          })}
-        />
-      </FormRow2> */}
       <FormRow label="Discount" error={errors?.discount?.message}>
         <Input
           type="number"
@@ -190,18 +138,6 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      {/* <FormRow2>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea
-          type="number"
-          id="description"
-          defaultValue=""
-          {...register("description", {
-            required: "This field is required",
-          })}
-        />
-      </FormRow2> */}
-
       <FormRow label="Description" error={errors?.description?.message}>
         <Textarea
           id="description"
@@ -214,16 +150,22 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label="Cabin photo">
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
-      <FormRow2>
+      <FormRow>
         {/* type is an HTML attribute! */}
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
         <Button disabled={isCreating}>Add cabin</Button>
-      </FormRow2>
+      </FormRow>
     </Form>
   );
 }
